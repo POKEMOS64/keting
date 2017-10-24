40720449acfe53dc9f2e58a49bbd0841274f833b
Sergey <polevoy.sergey777@gmail.com>
2017-10-24 12:31:52 +0400

    start

---
+++ dist/js/index.js
@@ -0,0 +1,9 @@
+$('.navi_cart_img').hover(
+	function(){
+		$(this).addClass('yellow');
+		
+	}, function(){
+		$(this).removeClass('yellow');
+		
+	}
+);
---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+﻿$('.navi_cart_img').hover(
+	function(){
+		$(this).addClass('yellow');
+		
+	}, function(){
+		$(this).removeClass('yellow');
+		
+	}
+);
